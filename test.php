d676d620845b1cf65664cb7d899ccc43472794a1
kishor shinde <kishor4shinde@gmail.com>
2022-06-04 01:05:56 +0530

    fsc

---
+++ test.php
@@ -0,0 +1,98 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+echo "CREATE TABLE userload0 ( PersonID int(100), userid int(100), ";
+for ($i = 0; $i <= 999; $i++) {
+    if ($i == 999) {
+        echo "`" . $i . "` int(100) ";
+    } else {
+        echo "`" . $i . "` int(100), ";
+    }
+}
+
+echo ");";
+echo "CREATE TABLE userload0 ( PersonID int(100), userid int(100), ";
+for ($i = 1000; $i <= 1999; $i++) {
+    if ($i == 1999) {
+        echo "`" . $i . "` int(100) ";
+    } else {
+        echo "`" . $i . "` int(100), ";
+    }
+}
+echo ");";
+echo "CREATE TABLE userload0 ( PersonID int(100), userid int(100), ";
+for ($i = 2000; $i <= 2999; $i++) {
+    if ($i == 2999) {
+        echo "`" . $i . "` int(100) ";
+    } else {
+        echo "`" . $i . "` int(100), ";
+    }
+}
+echo ");";
+echo "CREATE TABLE userload0 ( PersonID int(100), userid int(100), ";
+for ($i = 3000; $i <= 3999; $i++) {
+    if ($i == 3999) {
+        echo "`" . $i . "` int(100) ";
+    } else {
+        echo "`" . $i . "` int(100), ";
+    }
+}
+echo ");";
+echo "CREATE TABLE userload0 ( PersonID int(100), userid int(100), ";
+for ($i = 4000; $i <= 4999; $i++) {
+    if ($i == 4999) {
+        echo "`" . $i . "` int(100) ";
+    } else {
+        echo "`" . $i . "` int(100), ";
+    }
+}
+echo ");";
+echo "CREATE TABLE userload0 ( PersonID int(100), userid int(100), ";
+for ($i = 5000; $i <= 5999; $i++) {
+    if ($i == 5999) {
+        echo "`" . $i . "` int(100) ";
+    } else {
+        echo "`" . $i . "` int(100), ";
+    }
+}
+echo ");";
+echo "CREATE TABLE userload0 ( PersonID int(100), userid int(100), ";
+for ($i = 6000; $i <= 6999; $i++) {
+    if ($i == 6999) {
+        echo "`" . $i . "` int(100) ";
+    } else {
+        echo "`" . $i . "` int(100), ";
+    }
+}
+echo ");";
+echo "CREATE TABLE userload0 ( PersonID int(100), userid int(100), ";
+for ($i = 7000; $i <= 7999; $i++) {
+    if ($i == 7999) {
+        echo "`" . $i . "` int(100) ";
+    } else {
+        echo "`" . $i . "` int(100), ";
+    }
+}
+echo ");";
+echo "CREATE TABLE userload0 ( PersonID int(100), userid int(100), ";
+for ($i = 8000; $i <= 8999; $i++) {
+    if ($i == 8999) {
+        echo "`" . $i . "` int(100) ";
+    } else {
+        echo "`" . $i . "` int(100), ";
+    }
+}
+echo ");";
+echo "CREATE TABLE userload0 ( PersonID int(100), userid int(100), ";
+for ($i = 9000; $i <= 9999; $i++) {
+    if ($i == 9999) {
+        echo "`" . $i . "` int(100) ";
+    } else {
+        echo "`" . $i . "` int(100), ";
+    }
+}
+echo ");";
